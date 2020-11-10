c9ab87e344e4bc3ed9052735ac350e46a3dd2986
zavanton123 <zavanton123@gmail.com>
2020-11-10 16:15:47 +0300

    ViewSet used

--- university/urls.py
+++ university/urls.py
@@ -1,10 +1,25 @@
 from django.urls import path
 
-from university.views import StudentListView, StudentDetailView
+from university.views import StudentViewSet
 
 app_name = 'university'
 
+student_list = StudentViewSet.as_view(
+    {
+        'get': 'list',
+        'post': 'create'
+    }
+)
+
+student_detail = StudentViewSet.as_view(
+    {
+        'put': 'update',
+        'delete': 'destroy',
+        'get': 'retrieve'
+    }
+)
+
 urlpatterns = [
-    path('students/', StudentListView.as_view(), name='student-list'),
-    path('students/<int:pk>', StudentDetailView.as_view(), name='student-detail'),
+    path('students/', student_list, name='student-list'),
+    path('students/<int:pk>', student_detail, name='student-detail'),
 ]
--- university/views.py
+++ university/views.py
@@ -1,38 +1,11 @@
-from rest_framework import generics, mixins
+from rest_framework import viewsets
 
 from university.models import Student
 from university.serializers import StudentSerializer
 
 
-class StudentListView(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    generics.GenericAPIView
-):
+class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
-    def get(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return super().create(request, *args, **kwargs)
-
-
-class StudentDetailView(
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    generics.GenericAPIView
-):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-
-    def get(self, request, *args, **kwargs):
-        return super().retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return super().update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return super().destroy(request, *args, **kwargs)
