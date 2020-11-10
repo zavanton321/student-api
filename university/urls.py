08297134d7020f97f457e1ad7c24f692e6dfe0ef
zavanton123 <zavanton123@gmail.com>
2020-11-10 16:26:43 +0300

    api_root is added

--- university/urls.py
+++ university/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
-from university.views import StudentViewSet
+from university.views import StudentViewSet, api_root
 
 app_name = 'university'
+
+# router = routers.DefaultRouter()
+# router.register(prefix=r'students', viewset=StudentViewSet)
 
 student_list = StudentViewSet.as_view(
     {
@@ -20,6 +24,9 @@ student_detail = StudentViewSet.as_view(
 )
 
 urlpatterns = [
+    path('', api_root),
     path('students/', student_list, name='student-list'),
     path('students/<int:pk>', student_detail, name='student-detail'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- university/views.py
+++ university/views.py
@@ -1,11 +1,22 @@
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 from university.models import Student
 from university.serializers import StudentSerializer
 
 
+@api_view(http_method_names=['GET'])
+def api_root(request, format=None):
+    students = reverse(viewname='university:student-list', request=request, format=format)
+    return Response(
+        data={
+            'students': students
+        }
+    )
+
+
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-
-
