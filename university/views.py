5b8fe371ae49e341cacff1656c7a302440651205
zavanton123 <zavanton123@gmail.com>
2020-11-10 16:06:57 +0300

    mixins used

--- university/views.py
+++ university/views.py
@@ -1,29 +1,38 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
 
 from university.models import Student
 from university.serializers import StudentSerializer
 
 
-class StudentListView(generics.ListCreateAPIView):
+class StudentListView(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    generics.GenericAPIView
+):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
     def get(self, request, *args, **kwargs):
-        return super().get(request, *args, **kwargs)
+        return super().list(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        return super().post(request, *args, **kwargs)
+        return super().create(request, *args, **kwargs)
 
 
-class StudentDetailView(generics.RetrieveUpdateDestroyAPIView):
+class StudentDetailView(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    generics.GenericAPIView
+):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
     def get(self, request, *args, **kwargs):
-        return super().get(request, *args, **kwargs)
+        return super().retrieve(request, *args, **kwargs)
 
     def put(self, request, *args, **kwargs):
-        return super().put(request, *args, **kwargs)
+        return super().update(request, *args, **kwargs)
 
     def delete(self, request, *args, **kwargs):
-        return super().delete(request, *args, **kwargs)
+        return super().destroy(request, *args, **kwargs)
