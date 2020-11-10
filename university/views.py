d4c548e97b31c3820dbd7ca2ffa7e1a35c5806a4
zavanton123 <zavanton123@gmail.com>
2020-11-10 15:27:53 +0300

    get and post student list

--- university/views.py
+++ university/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
+from rest_framework.parsers import JSONParser
 
 from university.models import Student
 from university.serializers import StudentSerializer
@@ -15,6 +16,15 @@ def get_student_list(request):
     if request.method == 'GET':
         students = Student.objects.all()
         serializer = StudentSerializer(instance=students, many=True)
-        return JsonResponse(data=serializer.data, safe=False)
+        return JsonResponse(data=serializer.data, safe=False, status=200)
     elif request.method == 'POST':
-        pass
+        parsed = JSONParser().parse(stream=request)
+        serializer = StudentSerializer(data=parsed)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(data=serializer.data, status=201)
+        else:
+            return JsonResponse(data=serializer.errors, status=400)
+
+
+
