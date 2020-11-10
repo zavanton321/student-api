2937022f9add41fedbc468d75697d25f77a56f5f
zavanton123 <zavanton123@gmail.com>
2020-11-10 15:37:58 +0300

    student detail via function based views

--- university/urls.py
+++ university/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 
-from university.views import get_student_list
+from university.views import get_student_list, get_student_detail
 
 app_name = 'university'
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
-    path('', get_student_list, name='student-list'),
+    path('students', get_student_list, name='student-list'),
+    path('students/<int:pk>', get_student_detail, name='student-detail'),
 ]
--- university/views.py
+++ university/views.py
@@ -1,14 +1,10 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import TemplateView
 from rest_framework.parsers import JSONParser
 
 from university.models import Student
 from university.serializers import StudentSerializer
-
-
-class HomeView(TemplateView):
-    template_name = 'university/home.html'
 
 
 @csrf_exempt
@@ -27,4 +23,22 @@ def get_student_list(request):
             return JsonResponse(data=serializer.errors, status=400)
 
 
-
+@csrf_exempt
+def get_student_detail(request, pk):
+    if request.method == 'GET':
+        student = get_object_or_404(Student, pk=pk)
+        serializer = StudentSerializer(instance=student)
+        return JsonResponse(data=serializer.data, status=200)
+    elif request.method == 'PUT':
+        student = get_object_or_404(Student, pk=pk)
+        parsed = JSONParser().parse(stream=request)
+        serializer = StudentSerializer(instance=student, data=parsed)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(data=serializer.data, status=200)
+        else:
+            return JsonResponse(data=serializer.errors, status=400)
+    elif request.method == 'DELETE':
+        student = get_object_or_404(Student, pk=pk)
+        student.delete()
+        return HttpResponse(status=204)
